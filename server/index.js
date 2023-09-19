0970d94883eee41d45a2739e2019cd017a45061d
Deep Panchal <deep@fes.org.in>
2023-09-19 16:59:53 +0530

    added images and updated for live time message visiblity

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "/*",
+    origin: "http://localhost:3000",
     credentials: true,
   },
 });
